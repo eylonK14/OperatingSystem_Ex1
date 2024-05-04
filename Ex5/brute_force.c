fb20a38e96c9ff425b78b9e1c3dee18f54c9caa4
NoamLeshem <47627214+NoamLeshem@users.noreply.github.com>
2024-05-04 23:58:39 +0300

    finished Ex5

---
+++ Ex5/brute_force.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int brute_force(int *arr, int size)
+{
+	int best = 0;
+	for(int i = 0; i < size; i++) {
+		for(int j = i; j < size; j++) {
+			int sum = 0;
+			for(int k = i; k <= j; k++) {
+				sum += arr[k];
+			}
+			best = max(best, sum);
+		}
+	}
+	return best;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		printf("Usage: %s <seed> <array_size>\n", argv[0]);
+		return 1;
+	}
+	int seed = atoi(argv[1]);
+	int size = atoi(argv[2]);
+	int *arr = (int *)malloc(size * sizeof(int));
+	srand(seed);
+	for(int i = 0; i < size; i++) {
+		arr[i] = rand() % 100 - 25;
+	}
+	int best = brute_force(arr, size);
+	printf("Best sum: %d\n", best);
+	return 0;
+}
---
+++ Ex5/kadanes_alg.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int kadanes_alg(int *arr, int size)
+{
+	int max_sum = 0;
+	int current_sum = 0;
+	for(int i = 0; i < size; i++)
+	{
+		current_sum = max(arr[i], current_sum + arr[i]);
+		max_sum = max(max_sum, current_sum);
+	}
+	return max_sum;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		printf("Usage: %s <seed> <array_size>\n", argv[0]);
+		return 1;
+	}
+	int seed = atoi(argv[1]);
+	int size = atoi(argv[2]);
+	int *arr = (int *)malloc(size * sizeof(int));
+	srand(seed);
+	for(int i = 0; i < size; i++) {
+		arr[i] = rand() % 100 - 25;
+	}
+	int best = kadanes_alg(arr, size);
+	printf("Best sum: %d\n", best);
+	return 0;
+}
