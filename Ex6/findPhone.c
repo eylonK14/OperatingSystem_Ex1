afd1cfe3377d0fcd38cc55058f134b7ffb05cbbd
NoamLeshem <47627214+NoamLeshem@users.noreply.github.com>
2024-05-05 20:49:39 +0300

    refactor

--- Ex6/findPhone.c
+++ Ex6/findPhone.c
@@ -9,49 +9,50 @@ int main(int argc, char *argv[]) {
 	}
 	char *name = argv[1];
 	char *grepCmd[] = {"grep", name, "phonebook.txt", NULL};
-	char *cutCmd[] = {"cut", "-d", " ", "-f", "2", NULL};
-	int pipefd[2];
-	pipe(pipefd);
-	if (fork() == 0) {
-		close(pipefd[0]);
-		dup2(pipefd[1], 1);
-		close(pipefd[1]);
+	char *sedCmd[] = {"sed", "s/#/ /", NULL};
+	char *secondSedCmd[] = {"sed", "s/,/ /", NULL};
+	// we are doing this command: grep name phonebook.txt | sed 's/ /#/g' | sed 's/,/ /' | awk '{ print $2 }'
+	int pipe1[2];
+	int pipe2[2];
+	pipe(pipe1);
+	pipe(pipe2);
+	pid_t pid1 = fork();
+	if (pid1 == 0) {
+		dup2(pipe1[1], 1);
+		close(pipe1[0]);
+		close(pipe1[1]);
 		execvp(grepCmd[0], grepCmd);
-	} else {
-		close(pipefd[1]);
-		dup2(pipefd[0], 0);
-		close(pipefd[0]);
-		execvp(cutCmd[0], cutCmd);
 	}
-	// replace spaces with hashtag
-	int pipefd2[2];
-	pipe(pipefd2);
-	if (fork() == 0) {
-		close(pipefd2[0]);
-		dup2(pipefd2[1], 1);
-		close(pipefd2[1]);
-		char *sedCmd[] = {"sed", "s/#/ /", NULL};
+	pid_t pid2 = fork();
+	if (pid2 == 0) {
+		dup2(pipe1[0], 0);
+		dup2(pipe2[1], 1);
+		close(pipe1[0]);
+		close(pipe1[1]);
+		close(pipe2[0]);
+		close(pipe2[1]);
 		execvp(sedCmd[0], sedCmd);
-	} else {
-		close(pipefd2[1]);
-		dup2(pipefd2[0], 0);
-		close(pipefd2[0]);
 	}
-	// replace comma with space
-	int pipefd3[2];
-	pipe(pipefd3);
-	if (fork() == 0) {
-		close(pipefd3[0]);
-		dup2(pipefd3[1], 1);
-		close(pipefd3[1]);
-		char *secondSedCmd[] = {"sed", "s/,/ /", NULL};
+	pid_t pid3 = fork();
+	if (pid3 == 0) {
+		dup2(pipe2[0], 0);
+		close(pipe2[0]);
+		close(pipe2[1]);
 		execvp(secondSedCmd[0], secondSedCmd);
-	} else {
-		close(pipefd3[1]);
-		dup2(pipefd3[0], 0);
-		close(pipefd3[0]);
 	}
-	char *awkCmd[] = {"awk", "{print $2}", NULL};
-	execvp(awkCmd[0], awkCmd);
+	close(pipe1[0]);
+	close(pipe1[1]);
+	close(pipe2[0]);
+	close(pipe2[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	waitpid(pid3, NULL, 0);
+	char *awkCmd[] = {"awk", "{ print $2 }", NULL};
+	pid_t pid4 = fork();
+	if (pid4 == 0) {
+		dup2(0, 0);
+		execvp(awkCmd[0], awkCmd);
+	}
+	waitpid(pid4, NULL, 0);
 	return 0;
 }
