0bbcb092f76f1189b2d1b7c1bc97a9ff6eea4931
NoamLeshem <47627214+NoamLeshem@users.noreply.github.com>
2024-05-05 19:56:00 +0300

    added  validation fro add to phonebook

--- Ex6/add2PB.c
+++ Ex6/add2PB.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int validate(char *str)
+{
+    if (str[0] == ',')
+        return 0;
+    int i = 0;
+    for (i = 0; str[i] != ',' ;i++)
+    {
+        if ((str[i] < 'A' || str[i] > 'Z') && (str[i] < 'a' || str[i] > 'z'))
+            return 0;
+    }
+    i++;
+    int hypen_count = 0;
+    for (; str[i] != '\0'; i++)
+    {
+        if (str[i] == '-')
+        {
+            hypen_count++;
+            if (hypen_count > 1)
+                return 0;
+        }
+        else if (str[i] < '0' || str[i] > '9')
+            return 0;
+    }
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
+    // this program assumes that the input is enclosed in double quotes ,
+    // and the name and phone number are separated by a comma
     if (argc != 2)
     {
-        printf("Usage: %s <name>,<phone_number>\n", argv[0]);
+        printf("Usage: %s \"<name>,<phone_number>\"\n", argv[0]);
         return 1;
     }
-
+    if (!validate(argv[1]))
+    {
+        printf("Invalid input\n");
+        return 1;
+    }
     FILE *f = fopen("phonebook.txt", "a");
     if (f == NULL)
     {
@@ -16,6 +48,7 @@ int main(int argc, char *argv[])
         return 1;
     }
     // Write to file: name,phone_number
+
     fprintf(f, "%s\n", argv[1]);
     fclose(f);
 }
