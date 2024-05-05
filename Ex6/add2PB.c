e28a3f5867366ad828d76d64cf788b66db531431
NoamLeshem <47627214+NoamLeshem@users.noreply.github.com>
2024-05-05 19:22:11 +0300

    add to phonebook initial commit

--- Ex6/add2PB.c
+++ Ex6/add2PB.c
@@ -4,4 +4,18 @@
 int main(int argc, char *argv[])
 {
     if (argc != 2)
+    {
+        printf("Usage: %s <name>,<phone_number>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *f = fopen("phonebook.txt", "a");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    // Write to file: name,phone_number
+    fprintf(f, "%s\n", argv[1]);
+    fclose(f);
 }
